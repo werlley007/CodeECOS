0af0368d07100731605d1b586fb0979fb0e09686
werlley007 <thiagowerlley007@gmail.com>
2015-11-13 20:41:23 -0300

    Update data.h

--- ecos_master/include/data.h
+++ ecos_master/include/data.h
@@ -12,9 +12,14 @@ idxint *Gir = NULL;
 pfloat *Gpr = NULL;
 
 //Apr, Ajc, Air Arrays para a matriz A na coluna comprimido de armazenamento (CCS). Pode ser NULL se não há restrições de igualdade estão presentes
-idxint Ajc[3] = {0, 3, 6};	
-idxint Air[6] = {0, 1, 2, 0, 1, 3};	
-pfloat Apr[6] = {1, 1, 1, 1, 3, 1};	
+//idxint Ajc[3] = {0, 3, 6};	
+//idxint Air[6] = {0, 1, 2, 0, 1, 3};	
+//pfloat Apr[6] = {1, 1, 1, 1, 3, 1};	
+
+idxint Ajc[3] = {0, 2, 4};	
+idxint Air[4] = {0, 1, 0, 1};	
+pfloat Apr[4] = {1, 1, 1, 3};
+
 pfloat c[4] = {1, 2, 0, 0};	//c matriz de comprimento n
 pfloat *h = NULL;	//h matriz de comprimento m
 pfloat b[4] = {5, 6, 0, 0};	//b matriz de comprimento p. Pode ser NULL se não há restrições de igualdade estão presentes
